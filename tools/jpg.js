7cabcfa59ea36ac903b68416c900308f147aca81
switer <guankaishe@gmail.com>
2014-01-03 13:48:38 +0800

    add jpg support;

---
+++ tools/jpg.js
@@ -0,0 +1,27 @@
+var Jpgjs = require('../lib/jpgjs');
+
+/**
+ *  override
+ **/
+Jpgjs.prototype.load = function () {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", path, true);
+    xhr.responseType = "arraybuffer";
+    xhr.onload = (function() {
+        var data = new Uint8Array(xhr.response || xhr.mozResponseArrayBuffer);
+        this.parse(data);
+        if (this.onload)
+            this.onload();
+    }).bind(this);
+    xhr.send(null);
+}
+
+/**
+ *  
+ **/
+module.exports = {
+    parse: function () {
+        var img = new Jpgjs();
+
+    }
+}
